2794a8e79653223de6ddbd9f9a3953c810d3235f
Giannis Fotopoulos <jfotopoulos0@gmail.com>
2018-01-28 00:25:50 +0200

    Integrated technologies model

--- db/seeds.rb
+++ db/seeds.rb
@@ -54,3 +54,10 @@ end
 end
 
 puts "9 portfolio items created"
+
+3.times do |technology|
+	Portfolio.last.technologies.create!(
+		name: "Technology #{technology}"
+	)
+end
+puts "3 technologies created"
